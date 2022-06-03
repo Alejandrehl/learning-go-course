78adf50ebaa22dd72096947bff479b1bfafb02f7
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2022-06-03 15:32:37 -0400

    save

--- variables-types-values/short_operator.go
+++ variables-types-values/short_operator.go
@@ -5,9 +5,17 @@ import (
 	"strconv"
 )
 
+var z = 30
+
 func main() {
 	age := 27
 	message := "Hello, my name is Alejandro and I'm " + strconv.Itoa(age) + " years old."
+	number := getNumber()
 
 	fmt.Println(message)
+	fmt.Println(number)
+}
+
+func getNumber() int {
+	return z
 }
