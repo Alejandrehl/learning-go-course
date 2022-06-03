9c965f40a90c182ed007a7c62f11c3a883009933
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2022-06-03 14:47:28 -0400

    Concantenando string correctamente

--- variables-types-values/short_operator.go
+++ variables-types-values/short_operator.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	x1 := 42
+	age := 42
+	message := "Hello, my name is Alejandro and I'm " + strconv.Itoa(age) + " years old."
 
-	fmt.Println(x1)
+	fmt.Println(message)
 }
